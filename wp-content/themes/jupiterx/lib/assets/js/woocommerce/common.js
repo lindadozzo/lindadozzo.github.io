6f998317e1334a841cf734d83df1c9386a6e832e
okkinila <54004655+okkinila@users.noreply.github.com>
2020-04-29 11:31:11 +0200

    WP2Static CREATE wp-content/themes/jupiterx/lib/assets/js/woocommerce/common.js

---
+++ wp-content/themes/jupiterx/lib/assets/js/woocommerce/common.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function($) {
+
+  function InputSpinnerInit() {
+    $('.quantity > input').InputSpinner({
+      buttonsClass: 'btn-sm btn-outline-secondary',
+      buttonsWidth: 0
+    });
+  }
+
+  $(document).on('ready updated_wc_div', function() {
+    InputSpinnerInit();
+  });
+
+  // Quick cart view.
+  $(document).on('click', '.jupiterx-navbar-cart, .raven-shopping-cart', function(e) {
+    if ( '#' !== $(this).attr('href') ) {
+      return
+    }
+
+    e.preventDefault()
+    $('body').addClass('jupiterx-cart-quick-view-overlay')
+  })
+
+  $(document).on('click', '.jupiterx-mini-cart-close', function() {
+    $('body').alterClass('jupiterx-cart-quick-view-*', '')
+  })
+
+});
